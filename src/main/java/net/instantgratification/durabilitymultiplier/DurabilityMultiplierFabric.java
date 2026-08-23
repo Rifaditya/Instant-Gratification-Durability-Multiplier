bbc5eb2a63fd0fcfcd0a29b0dd99c1c507666efb
Rifa <fm.rifaditya@gmail.com>
2026-08-23 21:35:30 +0700

    Release v1.1.20+26.1.2: Auto-populate modded damageable items into config & live gamerule command recognition

--- src/main/java/net/instantgratification/durabilitymultiplier/DurabilityMultiplierFabric.java
+++ src/main/java/net/instantgratification/durabilitymultiplier/DurabilityMultiplierFabric.java
@@ -1,13 +1,20 @@
+// Copyright (C) 2026 Dasik (Rifaditya) | GNU GPLv3
 package net.instantgratification.durabilitymultiplier;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.instantgratification.durabilitymultiplier.config.DurabilityConfig;
 import net.instantgratification.durabilitymultiplier.network.DurabilityNetworking;
+import net.instantgratification.durabilitymultiplier.util.ModVersionGuard;
 
 public class DurabilityMultiplierFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ModVersionGuard.checkClass("Durability Multiplier", "net.minecraft.world.entity.EntityType");
         DurabilityMultiplier.init();
         DurabilityNetworking.registerServer();
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> DurabilityConfig.saveIfDirty());
+        ServerLifecycleEvents.SERVER_STOPPING.register(server -> DurabilityConfig.saveIfDirty());
     }
 }
